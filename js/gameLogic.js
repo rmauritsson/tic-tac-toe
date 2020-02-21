b0c0b9efddc9740330ad6a4dc6d6279854cbe9d7
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-02-21 16:22:59 -0600

    Fix logic errors

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -3,14 +3,16 @@
 /* eslint-disable no-unused-vars */
 
 const Board = () => {
-  let turn = 1;
   const emptySpace = 0;
-  const player1 = 1;
-  const player2 = 10;
+  const p1value = 1;
+  const p2value = -1;
+  let turn = 1;
   let board = Array(9).fill(emptySpace);
 
+  // Returns the Player number from the actual turn
   const playerTurn = () => (turn % 2 === 1 ? 1 : 2);
 
+  // Returns true if the board is empty
   const isEmpty = () => {
     for (let i = 0; i < board.length; i += 1) {
       if (board[i] !== emptySpace) return false;
@@ -19,6 +21,7 @@ const Board = () => {
     return true;
   };
 
+  // Returns true if the board is full
   const isFull = () => {
     for (let i = 0; i < board.length; i += 1) {
       if (board[i] === emptySpace) return false;
@@ -27,31 +30,43 @@ const Board = () => {
     return true;
   };
 
+  // Makes every space of the board empty and resets the turn to 1
   const resetBoard = () => {
     board = Array(9).fill(0);
     turn = 1;
   };
 
+  // Returns true if the position is valid for a new turn
   const validPosition = (position) => {
-    if (position < 0 || position > 8) return false;
-    if (isFull()) return false;
-    if (board[position] !== 0) return false;
+    if (position < 0 || position > 8) {
+      console.log('ERROR: Position can only be between 0 and 8');
+      return false;
+    }
+    if (isFull()) {
+      console.log('ERROR: The board is full, no more moves available');
+      return false;
+    }
+    if (board[position] !== 0) {
+      console.log('ERROR: Position already taken');
+      return false;
+    }
 
     return true;
   };
 
+  // Marks the board using the position argument. Returns true if a new move was successful.
   const newMove = (position) => {
-    if (validPosition(position) === false) {
-      console.log('ERROR: Invalid position');
-    } else {
-      board[position] = playerTurn === 1 ? player1 : player2;
-      turn += 1;
-    }
+    if (!validPosition(position)) return false;
+
+    board[position] = playerTurn() === 1 ? p1value : p2value;
+    turn += 1;
+
+    return true;
   };
 
-  // [0] [1] [2]
-  // [3] [4] [5]
-  // [6] [7] [8]
+  // Returns true if the sum of the 3 variables on a row,         [0] [1] [2]
+  // column or diagonal are equal to 3 or -3.                     [3] [4] [5]
+  // It should only be used by checkStatus                        [6] [7] [8]
   const checkWinner = (player, index) => {
     for (let i = 0; i < 3; i += 1) {
       if ((board[0 + (index * 3)] + board[1 + (index * 3)] + board[2 + (index * 3)] === player * 3)
@@ -70,8 +85,8 @@ const Board = () => {
   // returns 3 if there is a draw
   const checkStatus = () => {
     for (let i = 0; i < 3; i += 1) {
-      if (checkWinner(player1, i)) return 1;
-      if (checkWinner(player2, i)) return 2;
+      if (checkWinner(p1value, i)) return 1;
+      if (checkWinner(p2value, i)) return 2;
     }
 
     return isFull() ? 3 : 0;
@@ -86,7 +101,6 @@ const Board = () => {
     resetBoard,
     validPosition,
     newMove,
-    checkWinner,
     checkStatus,
   };
 };
