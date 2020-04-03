cc716b771b5d543670ec162a1ef4e1d34fcf7af7
Leo <ronald.mauritsson@gmail.com>
2020-04-03 23:20:39 +0300

    Tried mock functions

--- test/gameBoard.test.js
+++ test/gameBoard.test.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-undef */
 import GameBoard from '../src/js/gameBoard';
+import Player from '../src/js/player'
 
 it('returns the correct player\'s turn according to the turn number', () => {
-  const mockup1 = {
-    name: 'Sarah',
-    symbol: 'X',
-    getName(): 'Sarah';
-  };
-  const mockup2 = { name: 'John', symbol: 'O' };
-
-  const board = GameBoard(mockup1, mockup2);
-  expect(board.playerTurn(1)).toEqual('Sarah');
+  const obj = jest.genMockFromModule('../src/js/player')
+  console.log(obj.getMockName());
 });
